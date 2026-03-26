9b9e5646f9d2c258543f12991a7b9506e69dfd7a
sstelmaj <seba.stelmaj21@gmail.com>
2026-03-26 13:12:10 -0300

    Clean repo and apply .gitignore

--- build/resources/test/karate-config.js
+++
@@ -1,9 +0,0 @@
-function fn() {
-  var config = {
-    baseUrl: 'https://automationexercise.com'
-  };
-  karate.configure('ssl', true);
-  karate.configure('connectTimeout', 10000);
-  karate.configure('readTimeout', 10000);
-  return config;
-}
